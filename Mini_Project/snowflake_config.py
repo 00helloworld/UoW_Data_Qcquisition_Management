eeb549cb15cd5e15fad549885082432b550566b1
yanshen <yanshen0406@gmail.com>
2024-05-31 22:15:14 -0500

    ss

--- Mini_Project/snowflake_config.py
+++
@@ -1,7 +0,0 @@
-user = 'YANSHENDONG'
-password = 'Dd775852174110@'
-account = 'yxnfyva-zx64702'
-warehouse = None
-database = 'YANSHEN_DIT'
-schema = 'MINI_PROJECT'
-role = 'ACCOUNTADMIN'
--- class4/config.py
+++
@@ -1,7 +0,0 @@
-user = 'YANSHENDONG'
-password = 'Dd775852174110@'
-account = 'yxnfyva-zx64702'
-warehouse = None
-database = 'YANSHEN_DIT'
-schema = 'ETL_TASK'
-role = 'ACCOUNTADMIN'
